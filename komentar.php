47f6ad21b15c03cde257f9fdaf69e9666faf9bf9
Agung Miftakhul Huda <43521451+Agungmifta@users.noreply.github.com>
2018-10-05 10:21:55 +0700

    komentar.php

--- komentar.php
+++ komentar.php
@@ -1,3 +1,20 @@
-<?php
+<form method="POST">
+	<H1>KOMENTAR</H1>
+	<textarea name="KOMENTAR"></textarea><br>
+	<input type="submit" name="submit" value="SEND">
+</form>
 
-?>
+<?php 
+	if (isset($_POST['submit'])){
+		$komentar = $_POST['KOMENTAR'];
+
+		if (str_word_count($komentar)>= 5) {
+			echo "KOMENTAR";
+			echo $komentar;
+
+		}
+		else{
+			echo"harus memasukkan lebih dari 5 karakter";
+		}
+	}
+ ?>

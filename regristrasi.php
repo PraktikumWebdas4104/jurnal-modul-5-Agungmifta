1cb8dd0fe444ff3d1f6d16e7e93d104d08e7eaf1
Agung Miftakhul Huda <43521451+Agungmifta@users.noreply.github.com>
2018-10-05 10:24:42 +0700

    registrasi.php
    AGUNG MIFTAKHUL HUDA
    6701174089

--- regristrasi.php
+++ regristrasi.php
@@ -1,11 +1,36 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Form Registrasi</title>
-</head>
-<body>
+<form method="POST">
+	<table>
+		<tr>
+			<td>NIM</td>
+			<td></td>
+			<td><input type="text" name="nim"></td>
+		</tr>
+		<tr>
+			<td>Nama</td>
+			<td></td>
+			<td><input type="text" name="nama"></td>
+		</tr>
+		<tr>
+			<td>Email</td>
+			<td></td>
+			<td><input type="text" name="email"></td>
+		</tr>
+		<tr>
+			<td>Password</td>
+			<td></td>
+			<td><input type="password" name="pass"></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td></td>
+			<td><input type="submit" name="input" value="input">
+		</tr>
 
+	</table>
+</form>
 
-
-</body>
-</html>
+<?php 
+	if(isset($_POST['input'])){
+		include 'prosesregis.php';
+	}
+?>
